ac4dc5c1c705969b16d23f4f655a09d9de0b0532
Shell <72198971+Shell1010@users.noreply.github.com>
2022-01-23 19:14:42 +0000

    Merged filter and search

--- Osint/Resources/Browsers/google.py
+++ Osint/Resources/Browsers/google.py
@@ -6,21 +6,14 @@
 import aiohttp
 from bs4 import BeautifulSoup
 import asyncio
-###################################################
-class DuckDuck:
-    """ Class for DuckDuckGo searches and filtering searches to get URLs
+
+class Google:
+    """ Class for Google searches and filtering searches to get URLs
     """
     def __init__(self):
-        self.headers={
-            'accept': '*/*',
-            'origin': 'https://lite.duckduckgo.com',
-            'referer': 'https://lite.duckduckgo.com/',
-            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Brave Chrome/91.0.4472.124 Safari/537.36'
-        }
-        self.repeat = False
-        
+        self.headers={'User-Agent':'Mozilla/5.0 (X11; Linux x86_64; rv:96.0) Gecko/20100101 Firefox/96.0'}
 
-    async def search(self, query: str, amount: int = None):
+    async def search(self, query: str, amount: int=None):
         """This function searches and gets a text output for the specific search.
 
         Args:
@@ -32,32 +25,24 @@ class DuckDuck:
         """
         async with aiohttp.ClientSession() as session:
             if amount is None:
-                async with session.post(f"https://lite.duckduckgo.com/lite/", headers=self.headers, data={'q': query,'kl': None, 'dt': None }) as resp:
-                    text = await resp.text()
+                async with session.get(f"https://www.google.com/search?q={query}",headers=self.headers) as resp:
                     soup = BeautifulSoup(text, "html.parser")
-                    return [link.attrs['href'] for link in soup.find_all('a', rel='nofollow', href=True)]
+                    return [a_tags.find('a', href=True)['href'] for a_tags in soup.find_all('div', class_='yuRUbf')]
             else:
                 texts = []
                 links = []
-                s = 0
-                start = 0
-                async with session.post(f"https://lite.duckduckgo.com/lite/", headers=self.headers, data={'q': query,'kl': None, 'dt': None }) as resp:
-                    text = await resp.text()
-                    texts.append(text)
-                async with session.post(f"https://lite.duckduckgo.com/lite/", headers=self.headers, data={'q': query,'s': s+30, 'o':'json', 'dc':start+16, 'api':'d.js', 'kl':'wt-wt' }) as resp:
-                    text = await resp.text()
-                    texts.append(text)
-                for i in range(amount):
-                    async with session.post(f"https://lite.duckduckgo.com/lite/", headers=self.headers, data={'q': query,'s': s+50, 'o':'json', 'dc':start+50, 'api':'d.js', 'kl':'wt-wt' }) as resp:
+                start= 0
+                for i in range(amount+2):
+                    async with session.get(f"https://www.google.com/search?q={query}&start={start}",headers=self.headers) as resp:
                         text = await resp.text()
                         texts.append(text)
+                        start += 10
                 for text in texts:
                     soup = BeautifulSoup(text, "html.parser")
-                    for link in soup.find_all('a', rel='nofollow', href=True):
-                        links.append(link.attrs['href'])
+                    a_tags = soup.find_all('div', class_='yuRUbf')
+                    for link in a_tags:
+                        links.append(link.find('a', href=True)['href'])
                 return links
-
-
 
     # async def filter(self, resp: str):
     #     """Filters a response object so that only URL's for the specific search engine is listed
@@ -69,13 +54,19 @@ class DuckDuck:
     #         list: Returns a list of URLs
     #     """
     #     soup = BeautifulSoup(resp, "html.parser")
-    #     return [link.attrs['href'] for link in soup.find_all('a', rel='nofollow', href=True)]
+    #     return [a_tags.find('a', href=True)['href'] for a_tags in soup.find_all('div', class_='yuRUbf')]
 
-    
+###################################################
+# Dev Notes #
+###################################################
 # async def main():
-#     search = DuckDuck()
-#     resp = await search.search("site:'https://replit.com' intext:'selfbot'", 1)
+#     search = Google()
+#     resp = await search.search(query="site:'https://replit.com' intext:'selfbot'", amount=2)
 #     print(resp)
+
+
+            
 
 # if __name__ == '__main__':
 #     asyncio.run(main())
+###################################################
