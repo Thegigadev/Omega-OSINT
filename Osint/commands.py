aae7de2d03d15e0f25dd5ea31ed729a8f99e8c81
GigaDev <97202512+Thegigadev@users.noreply.github.com>
2022-01-23 16:36:44 -0500

    Update commands.py

--- Osint/commands.py
+++ Osint/commands.py
@@ -51,8 +51,6 @@ class run:
   {coloring.WHITE}-------------------------------------------------- 
   [{coloring.WARNING}!{coloring.WHITE}] Commands: {coloring.BLUE}[command name]{coloring.WHITE}
   {coloring.BLUE}Name:          {coloring.BLUE}Description:
-        
-<<<<<<< HEAD
   {coloring.WARNING}help        {coloring.WHITE}Creates specified number of GCs with tokens.
   {coloring.WARNING}clear       {coloring.WHITE}Clears terminal of all print. Alias cls.
   {coloring.WARNING}ascii       {coloring.WHITE}Prints terminal art.
@@ -61,17 +59,6 @@ class run:
   {coloring.WARNING}instagram   {coloring.WHITE}Searches Instagram for keyword.
   {coloring.WARNING}checkpwn    {coloring.WHITE}Check if email has been found in breaches.
   ---------------------------------------------------------------
-        """)
-=======
-  {coloring.WARNING}help            {coloring.WHITE}Creates specified number of GCs with tokens.
-  {coloring.WARNING}clear           {coloring.WHITE}Clears terminal of all print. Alias cls.
-  {coloring.WARNING}ascii           {coloring.WHITE}Prints terminal art.
-  {coloring.WARNING}exit            {coloring.WHITE}Terminates script.
-  {coloring.WARNING}twitter         {coloring.WHITE}Searches Twitter for keyword.
-  {coloring.WARNING}instagram       {coloring.WHITE}Searches Instagram for keyword.
-  {coloring.WARNING}facebook        {coloring.WHITE}Searches Facebook for keyword.
-  {coloring.WARNING}facebook api    {coloring.WHITE}Searches Facebook for keyword using their API.
-  --------------------------------------------------
         """)
 
     @social
@@ -89,7 +76,6 @@ class run:
     @social
     def facebook_account(query):
         asyncio.run(Facebook().search_account(query))
->>>>>>> 79993ba63140ba93bfc908842c40535cbd334690
 ###################################################
 # seriously well done very swag
 # Me > you? cry about it ok go work on facebook osint
