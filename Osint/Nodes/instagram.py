eb4b00e927a76517bb2a45bbfaec7810c9889a11
GigaDev <97202512+Thegigadev@users.noreply.github.com>
2022-01-24 11:02:29 -0500

    Credits
    - File made by Shell, revised by Roover.

--- Osint/Nodes/instagram.py
+++ Osint/Nodes/instagram.py
@@ -7,28 +7,37 @@ import aiohttp
 import asyncio
 from bs4 import BeautifulSoup
 from ..Resources.Browsers import *
+from ..Resources.design import coloring
 ###################################################
 class Instagram:
-    """
-        Search Instagram for keyword using search engines and dorking.
+	"""
+		Search Instagram for keyword using search engines and dorking.
 
-        Args:
+		Args:
 
-            Query (str): The keyword you wish to search.
+			Query (str): The keyword you wish to search.
 
-    """
-    def __init__(self):
-        self.google = Google()
-        self.yandex = Yandex()
-        self.duck = DuckDuck()
+	"""
+	
+	def __init__(self):
+		self.google = Google()
+		self.yandex = Yandex()
+		self.duck = DuckDuck()
 
-    async def search(self, query: str):
-        searches = await asyncio.gather(self.google.search(f"site:'https://www.instagram.com' intitle:'{query}'"), self.yandex.search(f"site:'https://www.instagram.com' intitle:'{query}'"), self.duck.search(f"site:'https://www.instagram.com' intitle:'{query}'"))
-        print(searches)
-        # text = await self.google.search(f"site:'https://www.instagram.com' intitle:'{query}'")
-        # urls.append(text)
-        # text = await self.yandex.search(f"site:'https://www.instagram.com' intitle:'{query}'")
-        # urls.append(text)
-        # text = await self.duck.search(f"site:'https://www.instagram.com' intitle:'{query}'")
-        # urls.append(text)
+	async def search(self, query: str):
+		searches = []
+		found = await self.google.search(f"site:'https://instagram.com' intitle:'{query}'")
+		for i in found:
+			searches.append(i)
+		found = await self.yandex.search(f"site:'https://instagram.com' intitle:'{query}'")
+		for i in found:
+			searches.append(i)
+		found = await self.duck.search(f"site:'https://instagram.com' intitle:'{query}'")
+		for i in found: 
+			searches.append(i)
+		for search in searches:
+			print(f"{coloring.FAIL}  -> Link found: {search}")
 ###################################################
+# Dev Notes #
+###################################################
+# File made by Shell, revised by Roover.
