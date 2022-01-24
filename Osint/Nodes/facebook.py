a66ba299c0189bf343222210df83e9c7e0ebe03b
GigaDev <97202512+Thegigadev@users.noreply.github.com>
2022-01-24 11:02:00 -0500

    Credits
    - File made by Shell, revised by Roover.

--- Osint/Nodes/facebook.py
+++ Osint/Nodes/facebook.py
@@ -8,46 +8,45 @@ from ..Resources.Browsers import *
 import asyncio
 import aiohttp
 from bs4 import BeautifulSoup
+from ..Resources.design import coloring
 ###################################################
 class Facebook:
-    """
-        Search Facebook for keyword using search engines and dorking.
+	"""
+		Search Facebook for keyword using search engines and dorking.
 
-        Args:
+		Args:
 
-            Query (str): The keyword you wish to search.
+			Query (str): The keyword you wish to search.
 
-    """
-    def __init__(self):
-        self.google = Google()
-        self.yandex = Yandex()
-        self.duck = DuckDuck()
+	"""
+	
+	def __init__(self):
+		self.google = Google()
+		self.yandex = Yandex()
+		self.duck = DuckDuck()
 
-    async def search2(self, query: str):
+	"""async def search2(self, query: str):
 
-        async with aiohttp.ClientSession() as session:
-            async with session.get(f"https://www.facebook.com/public/{query}?_fb_noscript=1") as resp:
-                text = await resp.text()
-                soup = BeautifulSoup(text, "html.parser")
-        print([url.attrs['href'] for url in soup.find_all('a', class_="_32mo", href=True)])
+		async with aiohttp.ClientSession() as session:
+			async with session.get(f"https://www.facebook.com/public/{query}?_fb_noscript=1") as resp:
+				text = await resp.text()
+				soup = BeautifulSoup(text, "html.parser")
+		print([url.attrs['href'] for url in soup.find_all('a', class_="_32mo", href=True)])"""
 
-    async def search(self, query: str):
-        """Search using search engines, uses dorking
-
-        Args:
-            query (str): The name/account you want to search
-
-        Returns:
-            list: Returns urls in list of lists
-        """
-        urls = []
-        searches = await asyncio.gather(self.google.search(f"site:'https://www.facebook.com' intitle:'{query}'"), self.yandex.search(f"site:'https://www.facebook.com' intitle:'{query}'"), self.duck.search(f"site:'https://www.facebook.com' intitle:'{query}'"))
-        print(searches)
-        # text = await self.google.search(f"site:'https://www.facebook.com' intitle:'{query}'")
-        # urls.append(text)
-        # text = await self.yandex.search(f"site:'https://www.facebook.com' intitle:'{query}'")
-        # urls.append(text)
-        # text = await self.duck.search(f"site:'https://www.facebook.com' intitle:'{query}'")
-        # urls.append(text)
-        print(urls)
+	async def search(self, query: str):
+		searches = []
+		found = await self.google.search(f"site:'https://facebook.com' intitle:'{query}'")
+		for i in found:
+			searches.append(i)
+		found = await self.yandex.search(f"site:'https://facebook.com' intitle:'{query}'")
+		for i in found:
+			searches.append(i)
+		found = await self.duck.search(f"site:'https://facebook.com' intitle:'{query}'")
+		for i in found: 
+			searches.append(i)
+		for search in searches:
+			print(f"{coloring.FAIL}  -> Link found: {search}")
 ###################################################
+# Dev Notes #
+###################################################
+# File made by Shell, revised by Roover.
