af2786e0695d85aa81f82dd366cd3bac8873e1b0
GigaDev <97202512+Thegigadev@users.noreply.github.com>
2022-01-24 11:03:54 -0500

    Credits
    - File made by Shell, revised by Roover.

--- Osint/Nodes/twitter.py
+++ Osint/Nodes/twitter.py
@@ -7,30 +7,37 @@ from bs4 import BeautifulSoup
 import aiohttp
 import asyncio
 from ..Resources.Browsers import *
+from ..Resources.design import coloring
 ###################################################
 class Twitter:
-    """
-        Search Twitter for keyword using search engines and dorking.
+	"""
+		Search Twitter for keyword using search engines and dorking.
 
-        Args:
+		Args:
 
-            Query (str): The keyword you wish to search.
+			Query (str): The keyword you wish to search.
 
-    """
-    def __init__(self):
-        self.google = Google()
-        self.yandex = Yandex()
-        self.duck = DuckDuck()
+	"""
+	
+	def __init__(self):
+		self.google = Google()
+		self.yandex = Yandex()
+		self.duck = DuckDuck()
 
-    async def search(self, query: str):
-        searches = await asyncio.gather(self.google.search(f"site:'https://twitter.com' intitle:'{query}'"), self.yandex.search(f"site:'https://twitter.com' intitle:'{query}'"), self.duck.search(f"site:'https://twitter.com' intitle:'{query}'"))
-        print(searches)
-        # urls = []
-        # text = await self.google.search(f"site:'https://twitter.com' intitle:'{query}'")
-        # urls.append(text)
-        # text = await self.yandex.search(f"site:'https://twitter.com' intitle:'{query}'")
-        # urls.append(text)
-        # text = await self.duck.search(f"site:'https://twitter.com' intitle:'{query}'")
-        # urls.append(text)
-        # print(urls)
+	async def search(self, query: str):
+		searches = []
+		found = await self.google.search(f"site:'https://twitter.com' intitle:'{query}'")
+		for i in found:
+			searches.append(i)
+		found = await self.yandex.search(f"site:'https://twitter.com' intitle:'{query}'")
+		for i in found:
+			searches.append(i)
+		found = await self.duck.search(f"site:'https://twitter.com' intitle:'{query}'")
+		for i in found: 
+			searches.append(i)
+		for search in searches:
+			print(f"{coloring.FAIL}  -> Link found: {search}")
 ###################################################
+# Dev Notes #
+###################################################
+# File made by Shell, revised by Roover.
